0c9784b58b275c902ac07b0223e6bc57a0449338
Anthony Buonomo <anthony.r.buonomo@nasa.gov>
2019-02-12 10:24:29 -0500

    update docker gpu

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN pip install jupyterlab & \
     pip install tqdm
 
 EXPOSE 8888
+EXPOSE 6006
 WORKDIR /home/
 ENV PYTHONPATH ="PYTHONPATH:/home/bert/:/home/src/"
 ENTRYPOINT ["jupyter", "lab","--ip=0.0.0.0","--allow-root"]
--- start_tensorflow-gpu.sh
+++ start_tensorflow-gpu.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#docker run -it -p 8887:8888 -p 6007:6006 \
-#    -v $(pwd)/:/home/ abuonomo/tensorflow
-docker run -it -p 8888:8888 -p 6006:6006 -v $(pwd)/:/home/ tensorflow/tensorflow:latest-gpu-py3-lab
+nvidia-docker run -it -p 8888:8888 -p 6006:6006 -v $(pwd)/:/home/ tensorflow/tensorflow:latest-gpu-py3-lab
