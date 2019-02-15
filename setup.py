e9582c2e2f8379a1650d781a66d46ddd653de42b
Anthony Buonomo <anthony.r.buonomo@nasa.gov>
2019-02-15 11:12:57 -0500

    fix reqs

--- setup.py
+++ setup.py
@@ -11,4 +11,11 @@ setup(
     author='Anthony Buonomo',
     author_email='anthony.r.buonomo@nasa.gov',
     packages=find_packages(where='./src'),
+    install_requires=[
+       'PyYAML>=3.13',
+       'tensorflow>=1.11.0',
+       'pandas>=0.23.4',
+       'numpy>=1.15.2',
+       'Flask>=1.0.2',
+    ],
 )
