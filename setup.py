f55479ebbf12d9ece028c966e4fd5572b176b314
Anthony Buonomo <anthony.r.buonomo@nasa.gov>
2019-02-15 11:31:37 -0500

    package path

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=readme,
     author='Anthony Buonomo',
     author_email='anthony.r.buonomo@nasa.gov',
-    packages=find_packages(where='./src'),
+    packages=find_packages(where='src'),
     install_requires=[
        'PyYAML>=3.13',
        'tensorflow>=1.11.0',
